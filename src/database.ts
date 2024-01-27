8ee1efa11830040301182e62d25736506e18a303
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-27 21:27:08 +0600

    feat: add getUserById func

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,16 @@
 import { User } from './models/User';
+import { v4 } from 'uuid';
+
 
 let users: User[] = [
     {
-        id: 1,
+        id: 'fc008863-aefb-4d8b-a015-ae58ab28f535',
         username: 'xu',
         age: 31,
         hobbies: ['reading', 'coding', 'yoga']
     },
     {
-        id: 2,
+        id: '64d45374-adbe-423a-ac6c-a84d3cf9108f',
         username: 'uisky',
         age: 44,
         hobbies: ['eat', 'sleep', 'vodka']
@@ -19,6 +21,11 @@ export function getUsers(): User[] {
     return users;
 }
 
+export function getUserById(id: string): User | undefined {
+    return users.find(user => user.id === id);
+}
+
 export function addUser(user: User): void {
+    user.id = v4();
     users.push(user);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import http from 'http';
-import { getUsers, addUser } from './database';
+import { parse } from 'url';
+import { getUsers, getUserById, addUser } from './database';
 import { User } from './models/User';
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/users' && req.method === 'GET') {
+  if (req.url) {
+    const { pathname } = parse(req.url, true);
+    if (pathname === '/users' && req.method === 'GET') {
       const users = getUsers();
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(users));
-    } else if (req.url === '/users' && req.method === 'POST') {
+    } else if (pathname?.startsWith('/users/') && req.method === 'GET') {
+      const id = pathname.split('/')[2];
+      const user = getUserById(id);
+      if (user) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(user));
+      } else {
+        res.statusCode = 404;
+        res.end('User not found');
+      }  
+    } else if (pathname === '/users' && req.method === 'POST') {
       let body = '';
       req.on('data', chunk => {
         body += chunk.toString();
@@ -26,6 +40,11 @@ const server = http.createServer((req, res) => {
       res.statusCode = 404;
       res.end('Not found');
   }
+  } else {
+    res.statusCode = 400;
+    res.end('Bad request');
+  }
+  
 });
 
 const port = process.env.PORT || 3000;
