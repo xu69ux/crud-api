f442c60a4ac05b674abc168bac05276b939c2040
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-27 21:09:43 +0600

    feat: add post user request handle

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import http from 'http';
-import { getUsers } from './database';
+import { getUsers, addUser } from './database';
+import { User } from './models/User';
 
 const server = http.createServer((req, res) => {
   if (req.url === '/users' && req.method === 'GET') {
@@ -10,6 +11,17 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(users));
+    } else if (req.url === '/users' && req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        const user: User = JSON.parse(body);
+        addUser(user);
+        res.statusCode = 201;
+        res.end('User added successfully');
+      });    
   } else {
       res.statusCode = 404;
       res.end('Not found');
