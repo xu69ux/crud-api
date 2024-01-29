311c05f9b4db9fc09538e484f494e7783858028e
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-30 00:37:37 +0600

    feat: update tests

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -10,6 +10,19 @@ describe('GET /users', () => {
     expect(Array.isArray(res.body)).toBe(true);
   });
 
+  it('GET /users/{userId} - should return the created user', async () => {
+    const res = await request(server).get(`/users/${userId}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id', userId);
+  });
+
+  it('GET /users/{userId} - should return 404 for deleted user', async () => {
+    const res = await request(server).get(`/users/${userId}`);
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
+describe('POST /users', () => {
   it('POST /users - should create a new user', async () => {
     const res = await request(server)
       .post('/users')
@@ -18,13 +31,9 @@ describe('GET /users', () => {
     expect(res.body).toHaveProperty('id');
     userId = res.body.id;
   });
+});
 
-  it('GET /users/{userId} - should return the created user', async () => {
-    const res = await request(server).get(`/users/${userId}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('id', userId);
-  });
-
+describe('PUT /users/{userId}', () => {
   it('PUT /users/{userId} - should update the user', async () => {
     const res = await request(server)
       .put(`/users/${userId}`)
@@ -32,15 +41,11 @@ describe('GET /users', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('username', 'Martin Alekseevich');
   });
-
-  it('DELETE /users/{userId} - should delete the user', async () => {
-    const res = await request(server).delete(`/users/${userId}`);
-    expect(res.statusCode).toEqual(204);
-  });
-
-  it('GET /users/{userId} - should return 404 for deleted user', async () => {
+});
+  
+describe('DELETE /users/{userId}', () => {
+  it('DELETE /users/{userId} - should return 404 for deleted user', async () => {
     const res = await request(server).get(`/users/${userId}`);
     expect(res.statusCode).toEqual(404);
   });
-
 });
