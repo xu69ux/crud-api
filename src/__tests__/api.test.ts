d8eff051e8921a0fdd223f154746a023abe96c8b
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-30 15:09:46 +0600

    feat: update tests

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import { server } from '../index';
 
-let userId: string;
-
 describe('GET /users', () => {
   it('should return a list of users', async () => {
     const res = await request(server).get('/users');
@@ -11,41 +9,69 @@ describe('GET /users', () => {
   });
 
   it('GET /users/{userId} - should return the created user', async () => {
-    const res = await request(server).get(`/users/${userId}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('id', userId);
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const responsePost = await request(server).post('/users').send(user);
+    const userId = responsePost.body.id;
+    const responseGet = await request(server).get(`/users/${userId}`);
+    expect(responseGet.statusCode).toEqual(200);
+    expect(responseGet.body).toHaveProperty('id', userId);
   });
 
   it('GET /users/{userId} - should return 404 for deleted user', async () => {
-    const res = await request(server).get(`/users/${userId}`);
-    expect(res.statusCode).toEqual(404);
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const responsePost = await request(server).post('/users').send(user);
+    const userId = responsePost.body.id;
+    await request(server).delete(`/users/${userId}`);
+    const responseGet = await request(server).get(`/users/${userId}`);
+    expect(responseGet.statusCode).toEqual(404);
   });
 });
 
 describe('POST /users', () => {
   it('POST /users - should create a new user', async () => {
-    const res = await request(server)
-      .post('/users')
-      .send({ username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] });
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const res = await request(server).post('/users').send(user);
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('id');
-    userId = res.body.id;
+  });
+
+  it('POST /users - should return 400 for invalid user data', async () => {
+    const user = { username: 'Martin Alekseevich', age: 30 };
+    const res = await request(server).post('/users').send(user);
+    expect(res.statusCode).toEqual(400);
   });
 });
 
 describe('PUT /users/{userId}', () => {
   it('PUT /users/{userId} - should update the user', async () => {
-    const res = await request(server)
-      .put(`/users/${userId}`)
-      .send({ username: 'Martin Alekseevich', age: 25, hobbies: ['read'] });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('username', 'Martin Alekseevich');
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const responsePost = await request(server).post('/users').send(user);
+    const userId = responsePost.body.id;
+    const responsePut = await request(server).put(`/users/${userId}`).send({ username: 'Martin' });
+    expect(responsePut.statusCode).toEqual(200);
   });
 });
-  
+
 describe('DELETE /users/{userId}', () => {
+  it('DELETE /users/{userId} - should return 400 for invalid user data', async () => {
+    const response = await request(server).delete('/users/invalid-id');
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it('DELETE /users/{userId} - should delete the user', async () => {
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const responsePost = await request(server).post('/users').send(user);
+    const userId = responsePost.body.id;
+    const responseDelete = await request(server).delete(`/users/${userId}`);
+    expect(responseDelete.statusCode).toEqual(204);
+  });
+
   it('DELETE /users/{userId} - should return 404 for deleted user', async () => {
-    const res = await request(server).get(`/users/${userId}`);
-    expect(res.statusCode).toEqual(404);
+    const user = { username: 'Martin Alekseevich', age: 30, hobbies: ['eat'] };
+    const responsePost = await request(server).post('/users').send(user);
+    const userId = responsePost.body.id;
+    await request(server).delete(`/users/${userId}`);
+    const responseDeleteAgain = await request(server).delete(`/users/${userId}`);
+    expect(responseDeleteAgain.statusCode).toEqual(404);
   });
 });
